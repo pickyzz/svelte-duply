019519e9464460a387582a5a0df609e496471fc5
Parinya T <pikcolokung@gmail.com>
2022-08-09 02:04:28 +0700

    theme

---
+++ src/lib/components/Darkmode.svelte
@@ -0,0 +1,9 @@
+<div class="fixed top-[2%] right-4">
+    <div>
+        <span class="mx-2">Theme</span>
+        <select data-choose-theme class="select select-bordered select-sm">
+            <option value="coffee">Dark</option>
+            <option value="garden">Light</option>
+        </select>
+    </div>
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,13 @@
 	import '../app.css';
 	import Kofi from '$lib/components/Kofi.svelte'
 	import ShareButton from '$lib/components/ShareButton.svelte'
+	import { themeChange } from 'theme-change'
+	import { onMount } from 'svelte'
+	import Darkmode from '$lib/components/Darkmode.svelte'
+
+	onMount(() => {
+		themeChange(false)
+	})
 
 	const url = 'https://pickyzz.dev'
 	const title = 'Sveltekit Template'
@@ -9,6 +16,7 @@
 </script>
 
 <main>
+	<Darkmode />
 	<Kofi name="pickyzz" label="Buy me Coffee" />
 	<ShareButton {url} {title} />
 	<slot />
